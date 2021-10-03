1e7cbe40ab9583d331d87137b7a115f5dac50fc6
arpitpatel <arpitpatel264@gmail.com>
2021-10-03 16:30:01 -0400

    added woodenspoon test

---
+++ src/tests/woodenSpoonTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class woodenSpoonTest {
+    public static void main(String[] args) {
+
+        for(int i =0 ; i <5; i++){
+            System.out.println("wooden Spoon");
+        }
+
+
+
+
+    }
+}
